f85ae49cf5d64f5db20f8549b9e9f2809b339385
hamphreytanatswa-collab <hamphreytanatswa@gmail.com>
2026-06-29 13:25:28 +0200

    Completed exercise 23

--- Javascript Fundamentals Exercises.js
+++ Javascript Fundamentals Exercises.js
@@ -184,6 +184,15 @@ switch(true){
         break;
     }
 
+    // 23 Conditionals:
+let score = Number(prompt("Enter your score:"));
+if (score >= 90) console.log("A");
+else if (score >= 80) console.log("B");
+else if (score >= 70) console.log("C");
+else console.log("Fail");
+
+
+
 
 
 
